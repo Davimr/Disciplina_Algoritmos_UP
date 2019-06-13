f30cee2c91f3276e416c9a1c8a864a1903c22b9e
Davi Michetti <43017919+Davimr@users.noreply.github.com>
2019-06-13 14:30:13 -0300

    Add files via upload

--- html2/scripts.js
+++ html2/scripts.js
@@ -1,3 +1,47 @@
+//Máscara do número de telefone
 $(document).ready(function(){
-  $('.numeroTelefone').mask('(99) 99999-9999');
+  $('#numeroTelefone').mask('(99) 99999-9999');
 });
+
+
+var senha1 = null, senha2 = null;
+
+var aoSair = function(){
+  senha1 = document.getElementById("primeiraSenha").value;
+  console.log(senha1);
+  if(senha1 != senha2){
+    document.getElementById('primeiraSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+    document.getElementById('segundaSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+  } else if (senha1 === senha2) {
+    document.getElementById('primeiraSenha').style.border = "1px solid #C0C0C0";
+    document.getElementById('segundaSenha').style.border = "1px solid #C0C0C0";
+  }
+}
+
+var aoSair2 = function(){
+  senha2 = document.getElementById("segundaSenha").value;
+  console.log(senha2);
+  if(senha1 != senha2){
+    document.getElementById('primeiraSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+    document.getElementById('segundaSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+  } else if (senha1 === senha2) {
+    document.getElementById('primeiraSenha').style.border = "1px solid #C0C0C0";
+    document.getElementById('segundaSenha').style.border = "1px solid #C0C0C0";
+  }
+}
+
+$('#formulario').submit(function() {
+    if (senha1 != senha2 || senha1 === null || senha2 === null){
+      document.getElementById('primeiraSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+      document.getElementById('segundaSenha').style.border = "1px solid rgba(188, 51, 51, 0.83)";
+      return false;
+    }
+});
+
+
+var drop = function(){
+  var dropinho = document.querySelector(".dropdown:hover .dropdown-content");
+  console.log(dropinho);
+  dropinho.style.display = "block";
+
+}
